76401f41aa224f1723ae9fd4150e4ad93c69baff
Jorge <jorge_fik@outlook.com>
2022-07-24 12:43:43 -0500

    udpate code carousel new version

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel,{ slidesToShowPlugin, slidesToScrollPlugin } from "@brainhubeu/react-carousel";
+import Carousel from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
 import Slides from "./Slides";
 import "./Slider.css";
@@ -10,45 +10,23 @@ const Slider = () => {
       <div className="carousel-title">
         <h2>My Projects</h2>
       </div>
-
       <Carousel
-            plugins={[
-                'arrows',
-                'infinite',
-                'centered',
-                {
-                    resolve: slidesToShowPlugin,
-                    options: {
-                    numberOfSlides: 2,
-                    },
-                },   
-                {
-                    resolve: slidesToScrollPlugin,
-                    options: {
-                    numberOfSlides: 2,
-                    },
-                },      
-            ]}
-            slidesPerPage={4}
-            animationSpeed={200}
-            offset={50}
-            itemWidth={400}
-            slides={Slides}
-            breakpoints={{
-              960:{
-                plugins: [
-                  
-                  {
-                    resolve: slidesToShowPlugin,
-                    options: {
-                      numberOfSlides: 1
-                    }
-                  },
-                ],
-                itemWidth:250,
-              }
-            }}
-        />
+        arrows
+        slidesPerPage={3}
+        infinite
+        animationSpeed={200}
+        centered
+        offset={50}
+        itemWidth={400}
+        slides={Slides}
+        breakpoints={{
+          960: {
+            slidesPerPage: 1,
+            arrows: false,
+            itemWidth: 250,
+          },
+        }}
+      />
     </div>
   );
 };
